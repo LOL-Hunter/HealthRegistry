6b41e0dfca9a25cd1379e80f79b46ccb9833beb7
LOL-Hunter <72355355+LOL-Hunter@users.noreply.github.com>
2023-08-21 13:51:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from backend import DataLoader, FoodTableDataSource, WidgetConfigurator, Utilities, TextViewDelegate
 import ui
-
+from console import hud_alert
 class GUI:
     GUI_INS = None
     def __init__(self):
         GUI.GUI_INS = self
-        self.selectedMode = None
+        self.selectedMode = "All"
         self.searchWord = ""
         self.foodData = DataLoader()
         self.loadView()
         self.fillListbox(self.foodData.getFoods())
+        self.updateInfo()
         #self.mainView.present("full_screen")
         self.navView.present("full_screen", hide_title_bar=True)
 
     def loadView(self):
         self.mainView = ui.load_view("main")
         self.propView = ui.load_view("prop")
+        self.daySView = ui.load_view("day_select")
 
         self.navView = ui.NavigationView(self.mainView)
         self.navView.width = 600
@@ -35,14 +37,27 @@ class GUI:
         seg.action = self.onSegChange
 
         search_field = self.mainView["search_field"]
-        search_field.delegate = TextViewDelegate()
+        search_field.delegate = TextViewDelegate(self.onSearch)
 
         clear_search = self.mainView["search_clear"]
         clear_search.action = self.clearSearch
 
-    def clearSearch(self):
+        show_act_test = self.mainView["show_active_test_button"]
+        show_act_test.action = self.showActiveTest
+
+    def clearSearch(self, e):
         search_field = self.mainView["search_field"]
         search_field.text = ""
+        self.updateListbox()
+
+    def showActiveTest(self, e):
+        if self.foodData.getActiveFood() is None:
+            hud_alert("Kein Test Aktiv!")
+            return 
+        self.navView.push_view(self.daySView)
+
+
+
 
     def onSearch(self, w):
         self.searchWord = w.text
@@ -53,7 +68,7 @@ class GUI:
         text = self.propView["prop_text_view"]
         text.text = prop
 
-        GUI.GUI_INS.navView.push_view(GUI.GUI_INS.propView)
+        self.navView.push_view(self.propView)
 
     def close(self, e):
         self.navView.close()
@@ -61,13 +76,14 @@ class GUI:
     def onSegChange(self, sender):
         sc = sender.superview['select_view_mode_seg']
         self.selectedMode = sc.segments[sc.selected_index]
+        self.updateListbox()
 
     def updateListbox(self):
-        if self.selectedMode == "All" and self.selectedMode != "All":
+        if self.selectedMode == "All":
             self.fillListbox(Utilities.search(self.foodData.getFoods(), self.searchWord))
-        elif self.selectedMode == "Untested" and self.selectedMode != "Untested":
+        elif self.selectedMode == "Untested":
             self.fillListbox(Utilities.search(self.foodData.getUnTestedFoods(), self.searchWord))
-        elif self.selectedMode == "Tested" and self.selectedMode != "Tested":
+        elif self.selectedMode == "Tested":
             self.fillListbox(Utilities.search(self.foodData.getTestedFoods(), self.searchWord))
 
     def fillListbox(self, _input:list):
