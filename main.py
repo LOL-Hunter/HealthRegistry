c013861f13b769b51e5fe491c45bdb7f655aa1a1
LOL-Hunter <72355355+LOL-Hunter@users.noreply.github.com>
2023-08-21 09:36:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class GUI:
         self.foodData = DataLoader()
         self.loadView()
         self.fillListbox(self.foodData.getFoods())
-        self.mainView.present("full_screen")
+        #self.mainView.present("full_screen")
 
     def loadView(self):
         self.mainView = ui.load_view("main")
@@ -18,8 +18,12 @@ class GUI:
         self.navView = ui.NavigationView(self.mainView)
         self.navView.width = 600
         self.navView.height = 400
-        self.navView.present("full_screen")
+        self.navView.present("full_screen", hide_title_bar=True)
 
+        close = ui.ButtonItem()
+        close.image = ui.Image.named('ionicons-close-24')
+        close.action = self.navView.close
+        self.mainView.left_button_items = [close]
 
 
        #prop = self.foodData.getProperties(selected)
