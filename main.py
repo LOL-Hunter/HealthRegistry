daa57521e62cbd06eac28bfc311e237ac99c4a4a
LOL-Hunter <72355355+LOL-Hunter@users.noreply.github.com>
2023-08-21 10:33:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class GUI:
     GUI_INS = None
     def __init__(self):
         GUI.GUI_INS = self
+        self.selectedMode = None
         self.foodData = DataLoader()
         self.loadView()
         self.fillListbox(self.foodData.getFoods())
@@ -27,8 +28,10 @@ class GUI:
 
         text = self.propView["prop_text_view"]
         WidgetConfigurator.configTextView(text)
+        WidgetConfigurator.configTextView(self.mainView["info_text_view"])
 
-
+        seg = self.mainView["select_view_mode_seg"]
+        seg.action = self.onSegChange
 
        #prop = self.foodData.getProperties(selected)
 
@@ -46,12 +49,29 @@ class GUI:
     def close(self, e):
         self.navView.close()
 
+    def onSegChange(self, sender):
+        sc = sender.superview['select_view_mode_seg']
+        selected_text = sc.segments[sc.selected_index]
+
+        if selected_text == "All" and self.selectedMode != "All":
+            self.selectedMode = "All"
+            self.fillListbox(self.foodData.getFoods())
+        elif selected_text == "Untested" and self.selectedMode != "Untested":
+            self.selectedMode = "Untested"
+            self.fillListbox(self.foodData.getUnTestedFoods())
+        elif selected_text == "Tested" and self.selectedMode != "Tested":
+            self.selectedMode = "Tested"
+            self.fillListbox(self.foodData.getTestedFoods())
+
     def fillListbox(self, _input:list):
         listBox = self.mainView["food_view"]
         listBox.data_source = GUI.CURRENT_FOOD_TABLE_DATA_SRC = FoodTableDataSource(self, _input)
         listBox.delegate = GUI.CURRENT_FOOD_TABLE_DATA_SRC
         listBox.reload_data()
 
+    def updateInfo(self):
+        text = self.mainView["info_text_view"]
+        text.text = self.foodData.getInfo()
 
 
 if __name__ == '__main__':
