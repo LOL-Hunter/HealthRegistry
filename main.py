884fbdc86beee1100b7be35cf632b520d079552a
LOL-Hunter <72355355+LOL-Hunter@users.noreply.github.com>
2023-08-21 09:31:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from backend import DataLoader, FoodTableDataSource
 import ui
 
 class GUI:
+    CURRENT_FOOD_TABLE_DATA_SRC = None
+    GUI_INS = None
     def __init__(self):
-
+        GUI.GUI_INS = self
         self.foodData = DataLoader()
         self.loadView()
         self.fillListbox(self.foodData.getFoods())
@@ -12,6 +14,12 @@ class GUI:
     def loadView(self):
         self.mainView = ui.load_view("main")
         self.propView = ui.load_view("prop")
+
+        self.navView = ui.NavigationView(self.mainView)
+        self.navView.width = 600
+        self.navView.height = 400
+        self.navView.present("full_screen")
+
 
 
        #prop = self.foodData.getProperties(selected)
@@ -22,14 +30,16 @@ class GUI:
 
     def fillListbox(self, _input:list):
         listBox = self.mainView["food_view"]
-        listBox.data_source = FoodTableDataSource(_input)
+        listBox.data_source = GUI.CURRENT_FOOD_TABLE_DATA_SRC = FoodTableDataSource(_input)
         listBox.reload_data()
 
 class Event:
     @staticmethod
     def onListBoxSelect(dataSource):
-        foodName = dataSource.name
-        
+        index = dataSource.selected_row
+        food = GUI.CURRENT_FOOD_TABLE_DATA_SRC.food[index]
+        GUI.GUI_INS.navView.push_view(GUI.GUI_INS.propView)
+
 
 
 
